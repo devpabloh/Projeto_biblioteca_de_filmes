657ea76344881b9e44cb48dd7f9555f851498886
Dev. Pablo H. <devpabloh@gmail.com>
2024-07-28 09:39:40 -0300

    atualização

---
+++ biblioteca-de-filmes/src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App/>}>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/movie/:id' element={<Movie/>}/>
+          <Route path='/search' element={<Search/>}/> {/* A busca será feita através de query string, que é uma parte da url que contém pares de chave-valor separados por & após um sinal de interrogação ? */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
